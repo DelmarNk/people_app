3ea29a322936da04c50cfda09681b652ef41c62f
delmarNK <delmarjinku3@gmail.com>
2024-03-28 23:49:40 +0800

    created CRUD routes

--- controllers/people_controller.js
+++ controllers/people_controller.js
@@ -7,6 +7,15 @@ router.get('/', (req,res)=>{
 router.post('/', (req,res)=>{
     res.status(201).json({message: 'people create route'})
 })
+router.get('/:id', (req,res)=>{
+    res.status(200).json({message: `people show route ${req.params.id}`})
+})
+router.delete('/:id', (req,res)=>{
+    res.status(200).json({message: `people delete route ${req.params.id}`})
+})
+router.put('/:id', (req,res)=>{
+    res.status(200).json({message: `people update route ${req.params.id}`})
+})
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV == "development"){
 }
 require('./db.connection')
 const PORT = process.env.PORT
-
+app.use(cors())
 app.use('/people', people_controller)
 
 app.get('/', (req,res)=>{
