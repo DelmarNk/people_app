7c12c7e1317535abbdd5da9add4e1211cfaf7408
delmarNK <delmarjinku3@gmail.com>
2024-04-25 13:26:50 +0800

    added a get route for a user

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,15 +2,20 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt') //require bcrypt installed(npm i bcrypt) to incrypt the password
-const {createUserToken} = require('../middleware/auth')
+const {createUserToken, requireToken} = require('../middleware/auth')
 
 router.post('/register', async (req,res)=>{
     try{
         const salt = await bcrypt.genSalt(10) // to create salt
         const passwordHash = await bcrypt.hash(req.body.password, salt) //to create the hashed password mixed with salt
+        const storePassword = req.body.password
         req.body.password = passwordHash //replace the old password by the new
         const user = await User.create(req.body)
-        res.status(201).json(user)
+        if (user){
+            req.body.password = storePassword
+            const token = await createUserToken(req, user)
+            res.status(201).json({user: user, isLoggedIn: true, token})
+        }
     } catch(error){
         res.status(400).json(error)
     }
@@ -24,6 +29,15 @@ router.post('/login', async (req,res)=>{
         res.status(200).json({user: foundUser, isLoggedIn: true, token})
     } catch(error){
         res.status(400).json(error.message)
+    }
+})
+
+router.get('/user/:id', requireToken, async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        res.status(201).json({id: user._id, username: user.username})
+    } catch(error){
+        res.status(400).json(error)
     }
 })
 
